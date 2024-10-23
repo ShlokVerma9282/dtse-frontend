e266c75bcac4827526b88306fa95538a30ee4f5a
Yogita Sawant <sawant.yogita1003@gmail.com>
2024-10-23 12:03:10 +0530

    added minor css changes

--- src/screens/Dashboard.tsx
+++ src/screens/Dashboard.tsx
@@ -6,7 +6,6 @@ import Procurement from "@/components/Procurement";
 import VendorPerformance from "@/components/VendorPerformance";
 
 export default function Dashboard() {
-
     const cardData = [
         {
             imageSrc: "/images/fiber-cards/1.png",
@@ -43,34 +42,48 @@ export default function Dashboard() {
     ];
 
     return (
-        <div className="bg-[#EEEEEE] p-6 min-h-screen">
-            <Header />
-            <h1 className="text-2xl font-bold mb-4">Dashboard</h1>
-            <p className="mb-6">Fiber Optics Orders</p>
-
-            <div className="grid grid-cols-1 md:grid-cols-4 gap-8">
-                {cardData.map((card, index) => (
-                    <FiberCard
-                        key={index}
-                        imageSrc={card.imageSrc}
-                        count={card.count}
-                        title={card.title}
-                        arrowType={card.arrowType}
-                        percentage={card.percentage}
-                        timeFrame={card.timeFrame}
-                    />
-                ))}
-            </div>
-            <div className="grid grid-cols-2 w-full gap-4">
-                <Procurement />
-                <VendorPerformance />
-                <div>
-                    <h1 className="text-2xl font-bold mb-4">Billing and Invoices</h1>
-                    <Billing/>
+        <div className="relative h-screen overflow-y-auto" >
+            <div className="relative z-10 bg-[#EEEEEE] p-6 min-h-screen">
+                <Header />
+                <div className="flex flex-row justify-between mt-4">
+                    <h1 className="text-2xl font-bold mb-4 font-barlow">Dashboard</h1>
+                    <div><img src="/images/filter.png" alt="" className="w-[250px] h-[65px]" /></div>
                 </div>
-                <div>
-                    <h1 className="text-2xl font-bold mb-4">Technical Team</h1>
-                    <Chart />
+
+                <p className="mb-6">Fiber Optics Orders</p>
+
+                <div className="grid grid-cols-1 md:grid-cols-4 gap-8">
+                    {cardData.map((card, index) => (
+                        <FiberCard
+                            key={index}
+                            imageSrc={card.imageSrc}
+                            count={card.count}
+                            title={card.title}
+                            arrowType={card.arrowType}
+                            percentage={card.percentage}
+                            timeFrame={card.timeFrame}
+                        />
+                    ))}
+                </div>
+                <div className="relative grid grid-cols-2 w-full gap-4">
+                    <div
+                        className="absolute inset-0 bg-no-repeat bg-cover bg-center opacity-10 z-0"
+                        style={{ backgroundImage: "url('/images/dashboard-bg.png')" }}
+                    ></div>
+                    <div className="relative z-10">
+                        <Procurement />
+                    </div>
+                    <div className="relative z-10">
+                        <VendorPerformance />
+                    </div>
+                    <div className="relative z-10">
+                        <h1 className="text-2xl font-bold mb-4">Billing and Invoices</h1>
+                        <Billing />
+                    </div>
+                    <div className="relative z-10">
+                        <h1 className="text-2xl font-bold mb-4">Technical Team</h1>
+                        <Chart />
+                    </div>
                 </div>
 
             </div>
